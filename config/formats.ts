b2402ae719ef7be558a10c19eff669c25f77f5fb
PrismaticShowdown <showdownprismatic@gmail.com>
2025-06-21 12:16:31 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -2491,7 +2491,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		mod: 'gen9',
 		ruleset: ['Standard NatDex', 'Terastal Clause'],
 		banlist: [
-			'ND Uber', 'ND AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+			'ND Uber', 'ND AG', 'IBL', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
 			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
 		],
 	},
